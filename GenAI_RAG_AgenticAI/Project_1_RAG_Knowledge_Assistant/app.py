5b342f9b20dc0c52c64eb922293deb17c67e2039
Sanjeev Kumar <sanjeev.ece.imps@gmail.com>
2026-03-01 16:56:25 +0530

    update app.py

--- GenAI_RAG_AgenticAI/Project_1_RAG_Knowledge_Assistant/app.py
+++ GenAI_RAG_AgenticAI/Project_1_RAG_Knowledge_Assistant/app.py
@@ -4,18 +4,19 @@ from src.utils.logger import logging
 from src.utils.exception import CustomException
 from app.streamlit_app import StreamlitApp
 
-"""
-Application Entry Point
-
-This file serves as the main execution entry for the Streamlit RAG application.
-
-Execution Flow:
-    1. Instantiate StreamlitApp
-    2. Load Streamlit UI
-    3. Handle unexpected runtime errors gracefully
-"""
-
 if __name__ == "__main__":
+    
+    """
+        Application Entry Point
+
+        This file serves as the main execution entry for the Streamlit RAG application.
+
+        Execution Flow:
+            1. Instantiate StreamlitApp
+            2. Load Streamlit UI
+            3. Handle unexpected runtime errors gracefully
+    """
+
     try:
         logging.info("Starting Streamlit RAG Application.")
 
