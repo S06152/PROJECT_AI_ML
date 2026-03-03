65d6ed66594e234be29e758727c2e8a7cc091926
Sanjeev Kumar <sanjeev.ece.imps@gmail.com>
2026-03-03 19:35:40 +0530

    Update

--- GenAI_RAG_AgenticAI/Project_1_RAG_Knowledge_Assistant/app/streamlit_app.py
+++ GenAI_RAG_AgenticAI/Project_1_RAG_Knowledge_Assistant/app/streamlit_app.py
@@ -115,7 +115,7 @@ class StreamlitApp:
 
                     except Exception as e:
                         logging.error(f"Failed to load {uploaded_file.name}: {e}")
-                        st.error(f"❌ Failed to load {uploaded_file.name}: {str(e)}")
+                        st.error(f"❌ Failed to load {uploaded_file.name}")
                         continue
 
                 if not all_documents:
